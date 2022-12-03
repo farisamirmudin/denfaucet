a10aabc0896d31b8757c739a6b2609a94dea5ea8
farisamirmudin <farisamirmudin@gmail.com>
2022-12-03 13:09:21 +0800

    change web title

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-
 import Body from '../components/Body'
-
 import Faq from '../components/Faq'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
@@ -13,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div id="top" className="bg-gradient-to-br from-[#7D77FF] to-[#FF9482] tracking-wider py-2">
       <Head>
-        <title>Den Token</title>
+        <title>Den Faucet</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
