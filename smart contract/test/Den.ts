be9bad79d80c363b4213a4f8cc89dd524a4c32b9
faris <faris071298@gmail.com>
2023-07-18 23:41:50 +0800

    Update test in smart contract

--- smart contract/test/Den.ts
+++ smart contract/test/Den.ts
@@ -11,34 +11,28 @@ describe("Den", () => {
   let reward = 50;
 
   beforeEach(async () => {
-    const Den = await ethers.getContractFactory("Den");
+    den = await ethers.deployContract("Den", [cap, reward]);
     [owner, address1, address2] = await ethers.getSigners();
-    den = await Den.deploy(cap, reward);
   });
 
   describe("Deployment", () => {
-    // set to the right owner
     it("Should set to the right owner", async () => {
       expect(await den.getOwner()).to.equal(owner.address);
     });
-    // assign the total supply of the token to the owner
     it("Should assign the total supply to the owner", async () => {
       expect(await den.totalSupply()).to.equal(
         await den.balanceOf(owner.address)
       );
     });
-    // set the max cap during deployment
     it("Should set the max cap during deployment", async () => {
       expect(Number(await den.cap()) / 10 ** 18).to.equal(cap);
     });
-    // set the block reward during deployment
     it("Should set the block reward during deployment", async () => {
       expect(Number(await den.getBlockReward()) / 10 ** 18).to.equal(reward);
     });
   });
   describe("Transaction", () => {
-    // transfer 100 tokens from owner to address1
-    it("Owner should transfer 100 tokens to address1, 50 tokens to address2 and check balance", async () => {
+    it("Owner should transfer 100 tokens to address1, 50 tokens to address2 and balance is 150 lesser", async () => {
       const initialBalance = await den.balanceOf(owner.address);
       await den.transfer(address1.address, 100);
       expect(await den.balanceOf(address1.address)).to.equal(100);
